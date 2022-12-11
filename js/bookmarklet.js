783b065bd35f81545043efc1fcc88614e111b58c
Omega Network Founder <94934670+GamingReborn@users.noreply.github.com>
2022-12-11 01:29:28 +0000

    Main commit

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,1 +1,1 @@
-(function() { var a = document.createElement('script'); a.src = 'https://mercury.justjxrdannnnnxn.repl.co/js/script.js'; document.body.appendChild(a); }())
+//javascript:(function () {var a = document.createElement('script');a.src = 'https://Mercury.justjxrdannnnnxn.repl.co/js/script.js';document.body.appendChild(a);}())
